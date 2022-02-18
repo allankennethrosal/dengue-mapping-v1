2dc4429cc32fcd0253d83319432d5b15f4b01b4c
allankennethrosal <rosal.allankenneth20@gmail.com>
2022-02-19 01:14:08 +0800

    First commit: Init react app and tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex flex-col justify-start items-start h-screen w-full">
+        <h1 className="text-xl text-white bg-gray-900 p-5 w-full">
+          Dengue Cases Mapping of Misamis Occidental
+        </h1>
+        <div className="flex-1 text-gray-100 bg-gray-800 p-5 h-full w-full">
+          asd
+        </div>
+      </div>
+    </>
   );
 }
 
