b71b2734a54e6dbdf39dd8f98830e731586a8220
allankennethrosal <rosal.allankenneth20@gmail.com>
2022-04-08 00:52:12 +0800

    Test API for retrieve cases

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import HomeMap from "./components/HomeMap";
 import Graphs from "./components/Graphs";
 import DataTable from "./components/DataTable";
+import { getCasesDB, createCaseDB } from "./utils/GlobalUtils";
+
+const data = {
+  Muncity: "OZAMIZ",
+  Streetpurok: "PUROK 4",
+  DateOfEntry: "2022-04-03T16:00:00.000Z",
+  DRU: "GOVERNMENT HOSPITAL",
+  AgeYears: 69,
+  AgeMons: 69,
+  AgeDays: 69,
+  Sex: "M",
+  AddressOfDRU: "UNAHAN SA AMOA",
+  ProvOfDRU: "MISAMIS OCCIDENTAL",
+  MuncityOfDRU: "OZAMIZ",
+  Admitted: 1,
+  DAdmit: "2022-04-03T16:00:00.000Z",
+  DOnset: "2022-04-03T16:00:00.000Z",
+  Type: "A",
+  LabRes: "PENDING",
+  CaseClassification: "IDK",
+  Outcome: "IDK",
+  RegionOfDrU: "10",
+  EPIID: "IDKASN1231oindasd0194usakdfn230",
+  Icd10Code: "IDK",
+  MorbidityMonth: 69,
+  MorbidityWeek: 69,
+  Year: 69,
+  NameOfDru: "MHARS GENERAL HOSPITAL",
+  ILHZ: "IDK",
+  District: "DISTRICT 2",
+  Barangay: "IDK"
+};
 
 function App() {
+  const [cases, setCases] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getCasesDB(setCases);
+    // createCaseDB(data, setError);
+  }, []);
+  console.log(cases);
+
   return (
     <>
       <Routes>
--- src/utils/GlobalUtils.js
+++ src/utils/GlobalUtils.js
@@ -1,3 +1,38 @@
+import axios from "axios";
+
+const ENDPOINT_DEV = "http://localhost/dengue-monitor";
+const ENDPOINT_PROD = "https://dengue-monitor.000webhostapp.com";
+
+export const getCasesDB = setCases => {
+  axios
+    .get(ENDPOINT_PROD + "/api/retrieve.php")
+    .then(response => {
+      // handle success
+      setCases(response.data);
+    })
+    .catch(error => {
+      // handle error
+      console.log(error);
+      setCases(null);
+    })
+    .then(() => {
+      // always executed
+    });
+};
+
+export const createCaseDB = (data, setError) => {
+  axios
+    .post(ENDPOINT_PROD + "/api/create.php", data)
+    .then(function(response) {
+      console.log(response);
+      setError(false);
+    })
+    .catch(function(error) {
+      console.log(error);
+      setError(true);
+    });
+};
+
 export const formatDate = rawDate => {
   return new Date(rawDate).toLocaleDateString(
     {},
